fbb040e7f1b57e82ac7dab1656d8d54466736462
Joel Caetano <kmpbits@gmail.com>
2025-06-06 14:29:23 +0100

    Add TodoItem composable

--- composeApp/src/commonMain/kotlin/com/joel/ktorfitdemo/App.kt
+++ composeApp/src/commonMain/kotlin/com/joel/ktorfitdemo/App.kt
@@ -1,12 +1,15 @@
 package com.joel.ktorfitdemo
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -16,6 +19,7 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import com.joel.ktorfitdemo.domain.model.Todo
 import com.joel.ktorfitdemo.domain.state.ResponseState
 import com.joel.ktorfitdemo.presentation.TodoViewModel
 import org.jetbrains.compose.ui.tooling.preview.Preview
@@ -49,11 +53,33 @@ fun App() {
                         verticalArrangement = Arrangement.spacedBy(10.dp)
                     ) {
                         items(todos) { todo ->
-                            Text(todo.title)
+                            TodoItem(
+                                modifier = Modifier.fillMaxWidth(),
+                                todo = todo
+                            )
                         }
                     }
                 }
             }
         }
+    }
+}
+
+@Composable
+private fun TodoItem(
+    modifier: Modifier = Modifier,
+    todo: Todo
+) {
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.spacedBy(10.dp)
+    ) {
+        Text(
+            text = todo.title,
+            style = MaterialTheme.typography.bodyLarge,
+            modifier = Modifier.padding(8.dp)
+        )
+
+        HorizontalDivider()
     }
 }
